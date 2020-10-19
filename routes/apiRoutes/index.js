67e53de403606518e3dcac95f47513d5fff57b7b
km4891 <kmorarity@gmail.com>
2020-10-18 17:37:36 -0700

    added post

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
-const notes = require('../../db/database');
+const database = require('../../db/database');
 
-router.get("/notes", function(req, res) {
-    notes
+router.get('/notes', function(req, res) {
+    database
       .allNotes()
       .then(notes => res.json(notes))
       .catch(err => res.status(500).json(err));
   });
+
+router.post('/notes', (req, res) => {
+    database
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch(err => res.status(500).json(err));
+})
 
 
 
