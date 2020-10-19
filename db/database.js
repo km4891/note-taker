311d5c6920bc3f2ba750c181a8cffe873f08f64d
km4891 <kmorarity@gmail.com>
2020-10-18 17:09:17 -0700

    add addNotes

--- db/database.js
+++ db/database.js
@@ -5,14 +5,49 @@ const readNotes = util.promisify(fs.readFile);
 const writeNotes = util.promisify(fs.writeFile);
 
 class Notes {
+    constructor() {
+        this.lastId = 0;
+    }
 
-allNotes() {
-    return readNotes('db/db.json', 'utf8')
-    .then(allNotes => {
-        // let data = [].concat(JSON.parse(allNotes))
-        return JSON.parse(allNotes);
-    })
-}
+    read() {
+        return readNotes('db/db.json', 'utf8');
+    }
+
+    write(note) {
+        return writeNotes('db/db.json', JSON.stringify(note));
+    }
+    
+    allNotes() {
+        return this.read()
+        .then(notes => {
+            let parsedNotes;
+            //if not an array or can't be turned into one, kick back empty array
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            }
+            catch (err) {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        });      
+    } 
+
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error("Title/Text cannot be empty!");
+        }
+
+        // Increment lastId and to assign it
+        const newNote = { title, text, id: ++this.lastId };
+
+        return this.allNotes()
+            .then(notes => [...notes, newNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => newNote);
+
+    }
 
 
 }
